bf5c574bf3f97cf3443f93c5a3013840db852e08
William Lamb <ucabwgl@ad.ucl.ac.uk>
2020-02-24 08:26:07 +0000

    include word_sim file

---
+++ intrinsic/word_sim.py
@@ -0,0 +1,82 @@
+'''
+Data set is from followinf paper:
+Lev Finkelstein, Evgeniy Gabrilovich, Yossi Matias, Ehud Rivlin, Zach Solan, Gadi Wolfman, and Eytan Ruppin. 2001. Placing search in context: the concept revisited. In Proc. of WWW.
+'''
+
+import numpy as np
+import scipy
+from scipy.stats import *
+import sys
+
+embeddingSource = sys.argv[1]
+embeddings = ""
+
+
+
+
+##### 1) loading the data
+
+def loadData():
+    "loads in the pre-trained embeddings; returns a dictionary mapping words to vectors"
+    ret = {}
+    data = open(embeddingSource,"r").readlines()
+    for row in data:
+        word = row.strip().split(' ')[0]
+        vals = row.strip().split(' ')[1:]
+        vals = np.array( [float(val) for val in vals] )
+        ret[word] = vals
+    return ret
+
+def loadTestData():
+    "loads in human-annotated word similarities"
+    tmp = open("word_sim.tab","r").readlines()
+    data = {}
+    data['words'] = [ row.strip().split('\t')[0:2] for i,row in enumerate(tmp) if i!=0 ]
+    data['sim_scores'] = [ float(row.strip().split('\t')[2]) for i,row in enumerate(tmp) if i!=0  ]
+    return data
+
+
+
+
+##### 2) getting similarity scores for word embeddings
+
+def getSimilarity(e1, e2):
+	"computes cosine similarity (cosine of angle between embedding vectors)"
+	return np.sum(e1 * e2)/( np.sqrt(np.sum(e1*e1)) * np.sqrt(np.sum(e2*e2)))
+
+def getSimilarityScoreForWords(w1,w2):
+    """for all words pairs in human-annotated data, returns cosine similarity of embedding vectors;
+    if either word not in embedding set, returns -1"""
+    global embeddings
+    #print w1
+    #print embeddings
+    if (w2 not in embeddings) or (w1 not in embeddings):
+        return -1
+    finalVector_w1 = embeddings[w1]
+    finalVector_w2 = embeddings[w2]
+    return getSimilarity(finalVector_w1, finalVector_w2)
+
+
+
+
+##### 3) computing correlation between human-annotated scores and cosine similarities for word embeddings
+
+def evaluate():
+    global embeddings
+    print("--- loading data...")
+    embeddings = loadData()
+    data = loadTestData()
+    print("words = ", len(data['words']))
+    print("scores = ", len(data['sim_scores']))
+    print("--- checking...")
+    pred_scores = []
+    invalid = 0
+    pred_scores = [ [getSimilarityScoreForWords(w1w2[0],w1w2[1]),human_score] for w1w2, human_score in zip(data['words'], data['sim_scores'])]
+    pred_scores = np.array( [ val for val in pred_scores if val[0]!=-1 ] )
+    spearman_rank_coeff,sp_rho = spearmanr( pred_scores[:,0], pred_scores[:,1] )
+    print("total, valid,spearman_rank_coeff,sp_rho ", len(data['words']),len(pred_scores), spearman_rank_coeff,sp_rho)
+	
+evaluate()
+
+
+
