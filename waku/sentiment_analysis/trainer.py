ad8ff4b2e8db5e6e86ceb6750e5361b403e70c6b
Kush Madlani <kushmadlani@kushs-macbook.mynet>
2020-03-29 11:23:34 +0100

    code documented

--- waku/sentiment_analysis/trainer.py
+++ waku/sentiment_analysis/trainer.py
@@ -11,16 +11,66 @@ from waku.sentiment_analysis.model import LSTMLearner
 from waku.sentiment_analysis.dataloader import SST
 from waku.sentiment_analysis.helpers import reduce_preprocess_embedding, pad_collate
 
-# function to save model parameters, which is used in early stopping to save the model
-# at the epoch with best validation accuracy, before the model overfits the training data
-def save_checkpoint(state, ep, filename='checkpoint.pth.tar'):
-    """Save checkpoint if a new best is achieved"""
-#     print ("=> Saving a new best at epoch:", ep)
-    torch.save(state, filename)  # save checkpoint
+
+def save_checkpoint(state_dict, filename='checkpoint.pth.tar'):
+    """
+    Save checkpoint of model used in early stopping 
+    Parameters:
+    -----------
+    state_dict : `dict`
+        PyTorch dictionary mapping model layer to tensor
+    filename : `str`
+        The path to save the checkpoints.
+    """
+    torch.save(state_dict, filename)
 
 def trainModel(embeddings_dict, embeddings_weights, trainData, valData, epochs=25, learning_rate=0.001, 
                batch_size=512, hidden_size=300, rnn_layers=1, mlp_layer_widths=100, checkpoint_filepath='checkpoint.pth.tar'):
-    
+    """
+    Train LSTMLearner model.
+
+    Parameters:
+    -----------
+    embeddings_dict : `dict`
+        Dict mapping words to int, which indexes row of weight matrix  
+    embeddings_weights : `numpy.ndarray``
+        (len(vocab), 300) Matrix of word embeddings  
+    trainData : `torch.Dataloader`
+        Batched training data
+    valData : `torch.Dataloader`
+        Batched validation data
+    epochs : `int`
+        Number of epochs for training.
+    learning_rate : `float`
+        The learning rate for backpropogation.        
+    batch_size : `int`
+        Number of word pairs in each batch.
+    hidden_size : `int`
+        Number of features in hidden state of LSTM
+    rnn_layers : `int`
+        Number of recurrent layers
+    mlp_layer_widths : `int`
+        Size of fully connected layer
+    checkpoint_filepath : `str`
+        The path to save the checkpoints.
+
+    Returns:
+    --------
+    model : `torch.Module`
+        Trained LSTMLearner model
+
+    losses : `list` of `float`
+        The total training loss in each epoch.
+
+    valLosses : `list` of `float`
+        The total validation loss in each epoch.
+
+    accuracy : `list` of `float`
+        The training accuracy in each epoch.
+
+    valAccuracy : `list` of `float`
+        The validation accuracy in each epoch.
+    """
     device = torch.device("cuda" if torch.cuda.is_available() else "cpu") 
     #create the data
     dataLoad = DataLoader(trainData, batch_size=batch_size, shuffle=True,
@@ -81,9 +131,9 @@ def trainModel(embeddings_dict, embeddings_weights, trainData, valData, epochs=2
             _, pred = out.max(1)
             valAccuracy.append(100*(pred == labels).sum().item()/len(valData))
 
-#         #if validation improved, save new best model
+        #if validation improved, save new best model
         if valAccuracy[-1] == max(valAccuracy):
-            save_checkpoint(model.state_dict(), ep, checkpoint_filepath)
+            save_checkpoint(model.state_dict(), checkpoint_filepath)
         epoch += 1
 
     #clean up
@@ -93,6 +143,23 @@ def trainModel(embeddings_dict, embeddings_weights, trainData, valData, epochs=2
     return model, losses, valLosses, accuracy, valAccuracy
 
 def plot_model(accuracy, valAccuracy, losses, valLosses):
+    """
+    Creates plots for accuracies, losses vs epoch.
+
+    Parameters:
+    -----------
+    losses : `list` of `float`
+        The total training loss in each epoch.
+
+    valLosses : `list` of `float`
+        The total validation loss in each epoch.
+
+    accuracy : `list` of `float`
+        The training accuracy in each epoch.
+
+    valAccuracy : `list` of `float`
+        The validation accuracy in each epoch.
+    """
     # epoch on which the best validation set accuracy occured
     bestEpoch = np.argmax(valAccuracy)
 
@@ -115,16 +182,40 @@ def plot_model(accuracy, valAccuracy, losses, valLosses):
 
 
 def test_model(model, TestData, accuracy, valAccuracy, print_accuracies=False, checkpoint_filepath="checkpoint.pth.tar"):
-  
+    """
+    Test LSTMLearner model on unseen test data.
+
+    Parameters:
+    -----------
+    model : `torch.Module`
+        Trained LSTMLearner model
+    TestData : `torch.Dataloader`
+        Batched test data
+    accuracy : `list` of `float`
+        The training accuracy in each epoch.
+    valAccuracy : `list` of `float`
+        The validation accuracy in each epoch.
+    print_accuracies : `bool`
+        Print out dict of training, validation, test accuracies
+    checkpoint_filepath : `str`
+        The path to load model parameters.
+
+    Returns:
+    --------
+    bestAccuracy : `dict`, {dataset, accuracy}
+        Dictionary of training, validation, test accuracies
+    """
     with torch.no_grad():
+        # load test data
         testDataLoad = DataLoader(TestData, batch_size=len(TestData),
                             collate_fn=pad_collate)
         ((text_pad, phrase_lengths), labels) = next(iter(testDataLoad))
         
+        # load trained model
         bestModel = copy.deepcopy(model)
         bestModel.load_state_dict(torch.load(checkpoint_filepath, map_location=torch.device("cpu")))
 
-        #set model to evaluation mode so dropout works as intended
+        # set model to evaluation mode so dropout works as intended
         bestModel.eval()
         out = bestModel(text_pad, phrase_lengths)
         _, pred = out.max(1)
@@ -144,25 +235,58 @@ def test_model(model, TestData, accuracy, valAccuracy, print_accuracies=False, c
     
     return bestAccuracy    
 
-# comment about 
+
 class Extrinsic_Sentiment_Analysis:
+    """
+    Experiment for sentiment analysis on Stanford Sentinent Treebank
+    """
     def __init__(self, SST, input_embedding_dict, input_embedding_weights):
-        
+        """
+        Arguments:
+        ----------
+        SST : `Class`
+            Data for experiment
+        input_embedding_dict : `dict`
+            Dict mapping words to int, which indexes row of weight matrix  
+        input_embedding_weights : `numpy.ndarray``
+            (len(vocab), 300) Matrix of word embeddings
+        """
         self.X_train = SST.X_train
         self.X_val = SST.X_val
         self.X_test = SST.X_test
         self.input_embedding_dict = input_embedding_dict
         self.input_embedding_weights = input_embedding_weights
         
+        # Reduce embedding to vocab in experiment
         self.embedding_dict, self.embedding_weights = reduce_preprocess_embedding(self.X_train, self.X_val, self.X_test, input_embedding_dict, input_embedding_weights)
 
+        # Process sentences of words into indices which map to embeddings
         SST.embed(self.embedding_dict)
         self.TrainData = SST.TrainData
         self.ValData = SST.ValData
         self.TestData = SST.TestData
 
     def train(self,epochs=100, learning_rate=0.001, batch_size=512, hidden_size=300, rnn_layers=2, mlp_layer_widths=100, checkpoint_filepath="checkpoint.pth.tar"):
-        """train LSTM model using AdamW optimiser with cross-entropy loss"""
+        """
+        Train LSTMLearner model.
+
+        Parameters:
+        -----------
+        epochs : `int`
+            Number of epochs for training.
+        learning_rate : `float`
+            The learning rate for backpropogation.        
+        batch_size : `int`
+            Number of word pairs in each batch.
+        hidden_size : `int`
+            Number of features in hidden state of LSTM
+        rnn_layers : `int`
+            Number of recurrent layers
+        mlp_layer_widths : `int`
+            Size of fully connected layer
+        checkpoint_filepath : `str`
+            The path to save the checkpoints.
+        """
         self.hidden_size = hidden_size
         self.rnn_layers = rnn_layers
         self.mlp_layer_widths = mlp_layer_widths
@@ -172,6 +296,28 @@ class Extrinsic_Sentiment_Analysis:
                                                                                 mlp_layer_widths=mlp_layer_widths, checkpoint_filepath=checkpoint_filepath)
 
     def test(self, print_accuracies=False, save_test_acc=False, file_path=None, checkpoint_filepath="checkpoint.pth.tar"):
+        """
+        Test LSTMLearner model on unseen test data.
+
+        Parameters:
+        -----------
+        model : `torch.Module`
+            Trained LSTMLearner model
+        TestData : `torch.Dataloader`
+            Batched test data
+        accuracy : `list` of `float`
+            The training accuracy in each epoch.
+        valAccuracy : `list` of `float`
+            The validation accuracy in each epoch.
+        print_accuracies : `bool`
+            Print out dict of training, validation, test accuracies
+        save_test_acc : `bool`
+            Save results
+        file_path : `str`
+            Path to save results of experiment
+        checkpoint_filepath : `str`
+            The path to load model parameters.
+        """
         try:
             self.bestAccuracy = test_model(self.model, self.TestData, self.accuracy, self.valAccuracy, print_accuracies=False, checkpoint_filepath=checkpoint_filepath)
         except:
@@ -186,12 +332,34 @@ class Extrinsic_Sentiment_Analysis:
 
 
     def plot(self):
+        """
+        Plots accurracies and losses
+        """
         try:
             plot_model(self.accuracy, self.valAccuracy, self.losses, self.valLosses)
         except:
-            raise ValueError('Model note trained')
+            raise ValueError('Model not trained')
             
 def evaluate(embedding_weights, data_filepath, embedding_dict, checkpoint_filepath):
+    """
+    Run downstream sentiment analysis experiment on a set of embeddings
+
+    Parameters:
+    -----------
+    embeddings_weights : `numpy.ndarray``
+        (len(vocab), 300) Matrix of word embeddings 
+    data_filepath : `str`
+        Path for raw Stanford Sentiment Treeback data
+    embedding_dict : `dict`
+        Dict mapping words to int, which indexes row of weight matrix  
+    checkpoint_filepath : `str`
+        The path to load model parameters.
+
+    Returns:
+    --------
+    Experiment.bestAccuracy["test set"] : `float`
+        Accuracy on test set
+    """
     # Instantiate a *SST* to load Stanford Sentiment Treebank train/test/val data
     SST_instance = SST(data_filepath)
 
